76c732ebc6eabfedc25d2ceb7b6986d6867e6a1c
Chimwemwe Shaba <chimwemweshaba@gmail.com>
2019-08-05 06:46:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/travelmantics/InsertActivity.java
@@ -0,0 +1,78 @@
+package com.example.travelmantics;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class InsertActivity extends AppCompatActivity
+{
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    EditText txtTitle;
+    EditText txtDescription;
+    EditText txtPrice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+
+        txtTitle = findViewById(R.id.txtTitle);
+        txtDescription = findViewById(R.id.txtDescription);
+        txtPrice = findViewById(R.id.txtPrice);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("traveldeals");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.save_menu:
+            {
+                saveDeal();
+                Toast.makeText(this, "Deal saved", Toast.LENGTH_LONG).show();
+                clean();
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveDeal()
+    {
+        String title = txtTitle.getText().toString();
+        String description = txtDescription.getText().toString();
+        String price = txtPrice.getText().toString();
+        TravelDeal deal = new TravelDeal(title, description, price, "");
+        mDatabaseReference.push().setValue(deal);
+    }
+
+    private void clean()
+    {
+        txtTitle.setText("");
+        txtDescription.setText("");
+        txtPrice.setText("");
+        txtTitle.requestFocus();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        return true;
+    }
+}
